3b7f117858ef42c9fdb33abeb17a5469d9eb0006
mahaabdulla <mahaabdulla87@gmail.com>
2025-01-15 06:22:47 +0300

    create web_services.g

--- lib/web_servicses.dart
+++ lib/web_servicses.dart
@@ -1,3 +1,20 @@
+
+import 'package:api_apps/users.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'web_servicses.g.dart';
+// this class 4 call apis
+
+@RestApi(baseUrl: 'https://gorest.co.in/public/v2/')
 abstract class WebServicses {
+   factory WebServicses(Dio dio, {String? baseUrl}) = _WebServicses;
+
+  // name of endpoint
+  @GET('users')
+  Future<List<Users>>getAllUsers();
   
 }
+
+
